a1395f525e68c9e00a69a774667f6c55dc1613c1
jmire <jmire@horizonconsultants.com>
2022-01-09 08:49:02 -0600

    Revert "bump version"
    This reverts commit 534f7ed0ae1f5d167c0de9bf7df7ae7e4be00932.

--- src/containers/Home/TokenDetails/index.js
+++ src/containers/Home/TokenDetails/index.js
@@ -49,7 +49,6 @@ const TokenDetails = (props) => {
     const [comparativeMarketCap, setComparativeMarketCap] = useState(0);
     useEffect(() => {
         (async () => {
-            // bump version
             const supply = await fetch(
                 `${config.REST_URL}/cosmos/bank/v1beta1/supply/${config.COIN_MINIMAL_DENOM}`,
             )
